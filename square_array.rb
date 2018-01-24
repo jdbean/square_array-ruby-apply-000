5f4d457b7c67113e808373d14f3a9b4bd20df627
J.D. Bean <jonathandbean@gmail.com>
2018-01-24 21:56:28 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,14 @@
+# Initial implementation
+=begin
 def square_array(array)
   squared_array = []
   array.each {|x| squared_array << x**2}
   return squared_array
+end
+=end
+
+# Advanced implementation
+
+def square_array(array)
+  square_array.collect {|x| x**2}
 end
